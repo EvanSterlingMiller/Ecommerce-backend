b91a180fd6507d824234a97e0ac153c68b4b3945
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-05-19 10:38:08 -0400

    updates

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,32 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: ['id', 'catagory_name'],
+    include: [
+      {
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      }
+    ]
+  })
+  .then((dbCategoryData) => res.json(dbCategoryData))
+  .catch((err) => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "category_name"]
+    
+  })
 });
 
 router.post('/', (req, res) => {
